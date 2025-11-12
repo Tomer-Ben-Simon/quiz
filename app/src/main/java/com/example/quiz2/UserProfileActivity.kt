9431d2c556aa1a76500a34969489ceffd0c45760
ASUS <Tomernirdit@hotmail.co.il>
2025-11-12 14:12:26 +0200

    new app quiz in parash first commit parashat chaye sara

--- app/src/main/java/com/example/quiz2/UserProfileActivity.kt
+++ app/src/main/java/com/example/quiz2/UserProfileActivity.kt
@@ -1,12 +1,7 @@
 package com.example.quiz2
-
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import java.util.UUID
 import android.widget.EditText
 import android.widget.Button
 import android.widget.TextView
